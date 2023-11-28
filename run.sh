7889e7dbca226bf452e4f1dc7765130395a850dc
250250 <ihobbang250@gmail.com>
2023-11-28 22:16:22 +0900

    initial commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+python3 ./predict_system.py \
+    --image_dir="./test_imgs" \
+    --det_algorithm="DB++" \
+    --det_model_dir="./model/detection" \
+    --rec_model_dir="./model/recognition" \
+    --rec_image_shape="3, 48, 320" \
+    --rec_char_dict_path="./ppocr/utils/korean_dict.txt" \
+    --use_gpu=False
