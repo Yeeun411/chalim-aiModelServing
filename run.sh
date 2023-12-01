974c8f003895957f4f5dc87d829d43f8c058755c
jungch15 <58167392+jungch15@users.noreply.github.com>
2023-12-02 00:00:13 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,8 +8,8 @@ python3 ./predict_system.py \
     --use_gpu=False
 
 python3 tools/infer/predict_system_num.py  \
-    --det_model_dir='./det_nummodel_dir/' \
-    --rec_model_dir='./rec_nummodel_dir/' \
+    --det_model_dir='./model/det_nummodel_dir/' \
+    --rec_model_dir='./model/rec_nummodel_dir/' \
     --image_dir="./image" \
     --rec_image_shape="3, 48, 320" \
     --draw_img_save_dir='./result/number/' \
