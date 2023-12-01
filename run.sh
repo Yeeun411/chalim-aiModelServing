293cc7ae185bc78f70bc9de05ed8673bcc5b8e6d
jungch15 <58167392+jungch15@users.noreply.github.com>
2023-12-01 23:56:43 +0900

    Update run.sh for number model

--- run.sh
+++ run.sh
@@ -6,3 +6,12 @@ python3 ./predict_system.py \
     --rec_image_shape="3, 48, 320" \
     --rec_char_dict_path="./ppocr/utils/korean_dict.txt" \
     --use_gpu=False
+
+python3 tools/infer/predict_system_num.py  \
+    --det_model_dir='./det_nummodel_dir/' \
+    --rec_model_dir='./rec_nummodel_dir/' \
+    --image_dir="./image" \
+    --rec_image_shape="3, 48, 320" \
+    --draw_img_save_dir='./result/number/' \
+    --rec_char_dict_path="./ppocr/utils/en_dict.txt" \
+    --use_gpu=False
