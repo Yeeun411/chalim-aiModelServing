8a7048fa9741361b936ab8e7a77613811c5b9a91
Yeaeun411 <yeaeun411@naver.com>
2023-12-06 22:07:29 +0900

    feat: django api

--- aiModelServing/servingApp/views.py
+++ aiModelServing/servingApp/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
-# Create your views here.
+import os
+import subprocess
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.core.files.storage import default_storage
+
+@csrf_exempt
+def image_upload(request):
+    if request.method == 'POST' and request.FILES['image']:
+        image_file = request.FILES['image']
+        file_path = os.path.join('aiModelServing', 'servingApp', 'aiModel', 'image', image_file.name)
+        file_name = default_storage.save(file_path, image_file)
+
+        subprocess.run(['bash', 'run.sh'])
+
+        results_path = os.path.join('aiModelServing', 'servingApp', 'aiModel', 'inference_results', 'final_results.txt')
+        with open(results_path, 'r') as file:
+            results = file.read()
+
+        return JsonResponse({'result': results})
+
+    return JsonResponse({'error': 'Invalid request'}, status=400)
